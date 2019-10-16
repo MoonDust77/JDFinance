b17b516dc92046ccbaec9e7ca7acf563274f4eac
qiqi <13759525126@163.com>
2019-10-16 14:03:37 +0800

    eslint 配置

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,18 +1,24 @@
 <template lang="html">
-    <div :class="$style.home">
-        <h1>Home</h1>
-    </div>
+  <div :class="$style.home">
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {}
+  },
+  created() {
+    let a = 1
+  }
 }
 </script>
 
 <style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
+@import "../../css/reset.scss";
+.home {
+  color: red;
+  font-size: 80px;
+}
 </style>
